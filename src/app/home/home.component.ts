b7d7668d47b97f75979792d12910908962003c63
fabio guelfi <fabioguelfiunix@gmail.com>
2018-07-04 01:26:11 -0300

    Exibindo ofertas com base no serviço OfertasService parte 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,19 @@
-import { OfertasService } from './../services/ofertas.service';
-import { Component, OnInit } from '@angular/core';
+import { Oferta } from "./../models/oferta.model";
+import { OfertasService } from "./../services/ofertas.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  public ofertas: Array<Oferta>;
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    console.log(this.ofertasService.getOfertas())
+    this.ofertas = this.ofertasService.getOfertas();
+    console.log(this.ofertas);
   }
-
 }
